c1cb193bb3c543ebf41079ad72aeee6fa4d4cf24
Amnaun <gong3264513407@outlook.com>
2025-04-08 21:24:14 +0800

    change in 2025/4/8 21:24

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -70,8 +70,51 @@ return {
     opts = {
       ---@type lspconfig.options
       servers = {
+        --clanged
+        clangd = {
+          keys = {
+            { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+          },
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "Makefile",
+              "configure.ac",
+              "configure.in",
+              "config.h.in",
+              "meson.build",
+              "meson_options.txt",
+              "build.ninja"
+            )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+              fname
+            ) or require("lspconfig.util").find_git_ancestor(fname)
+          end,
+          capabilities = {
+            offsetEncoding = { "utf-16" },
+          },
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+          },
+        },
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
+      },
+      setup = {
+        clangd = function(_, opts)
+          local clangd_ext_opts = LazyVim.opts("clangd_extensions.nvim")
+          require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+          return false
+        end,
       },
     },
   },
@@ -120,6 +163,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
+        "cpp",
         "bash",
         "html",
         "javascript",
@@ -187,6 +231,7 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        "codelldb",
         "stylua",
         "shellcheck",
         "shfmt",
