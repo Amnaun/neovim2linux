089c7ded20d7b7f6534489f26290d06cecb12b6f
Amnaun <gong3264513407@outlook.com>
2025-04-08 17:29:53 +0800

    change

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("lazy").setup({
+  spec = { { "folke/lazy.nvim", version = "*" } },
+  git = { url = "git@github.com:%s.git" },  -- 强制使用 SSH
+})
