1e1996dbadb6b9ce96d68ec9cdd67012cfedde70
Amnaun <gong3264513407@outlook.com>
2025-04-09 02:36:28 +0800

    change 2025/4/9 02:15

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -2,14 +2,16 @@ return {
   "mistricky/codesnap.nvim",
   build = "make build_generator",
   requires = { "nvim-treesitter/nvim-treesitter" },
-  require("codesnap").setup({
-    has_line_number = true,
-    bg_theme = "dusk",
-    has_breadcrumbs = true,
-    show_workspace = true,
-    breadcrumbs_separator = "🌟",
-    watermark = "Amnaun",
-  }),
+  config = function()
+    require("codesnap").setup({
+      has_line_number = true,
+      bg_theme = "dusk",
+      has_breadcrumbs = true,
+      show_workspace = true,
+      breadcrumbs_separator = "🌟",
+      watermark = "Amnaun",
+    })
+  end,
   opts = {
     save_path = "~/Pictures",
     has_breadcrumbs = true,
