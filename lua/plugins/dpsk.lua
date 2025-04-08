3f6c6a4d11575e2d0d7bf0fec5e06ad481ae7c62
Amnaun <gong3264513407@outlook.com>
2025-04-08 23:40:25 +0800

    change 2025/4/8 23:40

--- lua/plugins/dpsk.lua
+++ lua/plugins/dpsk.lua
@@ -1,56 +1,104 @@
 return {
-  {
-    "yetone/avante.nvim",
-    custom = true,
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      provider = "deepseek",
-      vendors = {
-        deepseek = {
-          __inherited_from = "openai",
-          api_key_name = "DEEPSEEK_API_KEY",
-          endpoint = "https://api.deepseek.com",
-          model = "deepseek-coder",
-        },
+  "yetone/avante.nvim",
+  custom = true,
+  event = "VeryLazy",
+  -- lazy = false,
+  -- version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  opts = {
+    -- add any opts here
+    -- for example
+    provider = "deepseek",
+    vendors = {
+      deepseek = {
+        __inherited_from = "openai",
+        api_key_name = "DEEPSEEK_API_KEY",
+        endpoint = "https://api.deepseek.com",
+        model = "deepseek-coder",
       },
     },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
+    windows = {
+      sidebar_header = {
+        rounded = false,
+      },
+      edit = {
+        border = "single",
+      },
+      ask = {
+        border = "single",
+      },
+    },
+    file_selector = {
+      provider = "fzf", -- Avoid native provider issues
+      provider_opts = {},
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+    or "http_proxy=127.0.0.1:7890 https_proxy=127.0.0.1:7890 make",
+  dependencies = {
+    -- "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    -- The below dependencies are optional,
+    -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    "echasnovski/mini.icons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    "HakonHarnes/img-clip.nvim", -- support for image pasting
+    -- "MeanderingProgrammer/render-markdown.nvim",  -- Make sure to set this up properly if you have lazy=true
+    {
+      "saghen/blink.cmp",
+      opts = {
+        sources = {
+          default = { "avante_commands", "avante_mentions", "avante_files" },
+          compat = {
+            "avante_commands",
+            "avante_mentions",
+            "avante_files",
+          },
+          -- LSP score_offset is typically 60
+          providers = {
+            avante_commands = {
+              name = "avante_commands",
+              module = "blink.compat.source",
+              score_offset = 90,
+              opts = {},
             },
-            -- required for Windows users
-            use_absolute_path = true,
+            avante_files = {
+              name = "avante_files",
+              module = "blink.compat.source",
+              score_offset = 100,
+              opts = {},
+            },
+            avante_mentions = {
+              name = "avante_mentions",
+              module = "blink.compat.source",
+              score_offset = 1000,
+              opts = {},
+            },
           },
         },
       },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
+    },
+    {
+      "saghen/blink.compat",
+      opts = function()
+        -- monkeypatch cmp.ConfirmBehavior for Avante
+        require("cmp").ConfirmBehavior = {
+          Insert = "insert",
+          Replace = "replace",
+        }
+      end,
+    },
+    {
+      "folke/which-key.nvim",
+      optional = true,
+      opts = {
+        spec = {
+          { "<leader>a", group = "ai" },
         },
-        ft = { "markdown", "Avante" },
       },
     },
   },
